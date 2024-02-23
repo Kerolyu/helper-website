7818a1cd8556d4125bc7f863d2f9cbb3fb6c859a
Yulia <kerolyu14@gmail.com>
2024-02-23 12:04:04 +0200

    js- tabs logic

--- js/modal.js
+++ js/modal.js
@@ -2,6 +2,8 @@ const closeModalButton = document.querySelector(".close-modal");
 const backgroundBlur = document.querySelector(".background-blur");
 const modalLogIn = document.querySelector(".log-in-modal");
 const openModalButton = document.querySelector(".login-button");
+const modalTabsButtons = document.querySelectorAll(".modal-titles__option");
+const tabs = document.querySelectorAll(".modal-tab");
 
 function toggleModalVisibility() {
   modalLogIn.classList.toggle("modal-hidden");
@@ -11,3 +13,21 @@ function toggleModalVisibility() {
 closeModalButton.addEventListener("click", toggleModalVisibility);
 openModalButton.addEventListener("click", toggleModalVisibility);
 backgroundBlur.addEventListener("click", toggleModalVisibility);
+
+modalTabsButtons.forEach((button, index) => {
+  button.addEventListener("click", () => {
+    modalTabsButtons.forEach((Button) => {
+      if (Button !== button) {
+        Button.classList.remove("login-active");
+      }
+    });
+    button.classList.add("login-active");
+    tabs.forEach((tab, tabIndex) => {
+      if (tabIndex === index) {
+        tab.classList.toggle("hidden");
+      } else if (tabIndex !== index) {
+        tab.classList.add("hidden");
+      }
+    });
+  });
+});
